5d5f406e2c05ef0fc2e817c74fbc59ac6b45a1e6
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-03-02 23:51:37 -0800

    updated music-api

--- handler.js
+++ handler.js
@@ -11,7 +11,6 @@ const SQS_QUEUE_URL =
 
 const app = express();
 
-
 function queryDynamoDb(params) {
   const documentClient = new AWS.DynamoDB.DocumentClient({
     region: "us-east-1",
@@ -93,158 +92,88 @@ app.get("/genres", function (req, res) {
     });
 });
 
-app.get(
-  "/artists/for/genre",
-  [
-    query("genre", "Missing genre query parameter").exists({
-      checkFalsy: true,
-    }),
-  ],
-  (req, res) => {
-    const validationErrors = validationResult(req);
-    if (!validationErrors.isEmpty()) {
-      return res.status(400).send(validationErrors.array());
-    }
+app.get("/artists/for/genre", function (req, res) {
+  const params = {
+    TableName: MUSIC_TABLE,
+    IndexName: "artist_gsi",
+  };
 
-    const genre = req.query.genre;
+  scanDynamoDb(params)
+    .then((items) => {
+      if (items.Count < 1) {
+        return res.status(404).send("No genres found");
+      }
+      return res
+        .status(200)
+        .send(_.uniq(_.map(items.Items, (item) => item.artist)));
+    })
+    .catch((err) => {
+      return res.status(500).send(err);
+    });
+});
 
-    const params = {
-      TableName: MUSIC_TABLE,
-      KeyConditionExpression: "genre = :genre",
-      ExpressionAttributeValues: {
-        ":genre": genre,
-      },
-    };
+app.get("/albums/for/artist", function (req, res) {
+  const params = {
+    TableName: MUSIC_TABLE,
+    IndexName: "artist_gsi",
+  };
 
-    queryDynamoDb(params)
-      .then((items) => {
-        if (items.Count < 1) {
-          return res.status(404).send(`No artists found for genre ${genre}`);
-        }
-        return res
-          .status(200)
-          .send(_.uniq(_.map(items.Items, (item) => item.artist)));
-      })
-      .catch((err) => {
-        return res.status(500).send(err);
-      });
-  }
-);
+  scanDynamoDb(params)
+    .then((items) => {
+      if (items.Count < 1) {
+        return res.status(404).send("No genres found");
+      }
+      return res
+        .status(200)
+        .send(_.uniq(_.map(items.Items, (item) => item.album)));
+    })
+    .catch((err) => {
+      return res.status(500).send(err);
+    });
+});
 
-// app.get(
-//   "/albums/for/artist",
-//   [
-//     query("artist", "Missing artist query parameter").exists({
-//       checkFalsy: true,
-//     }),
-//   ],
-//   (req, res) => {
-//     const validationErrors = validationResult(req);
-//     if (!validationErrors.isEmpty()) {
-//       return res.status(400).send(validationErrors.array());
-//     }
+app.get("/songs/for/album", function (req, res) {
+  const params = {
+    TableName: MUSIC_TABLE,
+    IndexName: "artist_gsi",
+  };
 
-//     const artist = req.query.artist;
+  scanDynamoDb(params)
+    .then((items) => {
+      if (items.Count < 1) {
+        return res.status(404).send("No genres found");
+      }
+      return res
+        .status(200)
+        .send(_.uniq(_.map(items.Items, (item) => item.song)));
+    })
+    .catch((err) => {
+      return res.status(500).send(err);
+    });
+});
 
-//     const params = {
-//       TableName: MUSIC_TABLE,
-//       IndexName: "artistAlbum",
-//       KeyConditionExpression: "artist = :artist",
-//       ExpressionAttributeValues: {
-//         ":artist": artist,
-//       },
-//     };
+app.get("/song", function (req, res) {
+  const song = req.query.song;
 
-//     queryDynamoDb(params)
-//       .then((items) => {
-//         if (items.Count < 1) {
-//           return res.status(404).send(`No albums found for artist ${artist}`);
-//         }
-//         return res
-//           .status(200)
-//           .send(_.uniq(_.map(items.Items, (item) => item.album)));
-//       })
-//       .catch((err) => {
-//         return res.status(500).send(err);
-//       });
-//   }
-// );
+  const params = {
+    TableName: MUSIC_TABLE,
+    IndexName: "artist_gsi",
+  };
 
-// app.get(
-//   "/songs/for/album",
-//   [
-//     query("album", "Missing album query parameter").exists({
-//       checkFalsy: true,
-//     }),
-//   ],
-//   (req, res) => {
-//     const validationErrors = validationResult(req);
-//     if (!validationErrors.isEmpty()) {
-//       return res.status(400).send(validationErrors.array());
-//     }
-
-//     const album = req.query.album;
-
-//     const params = {
-//       TableName: MUSIC_TABLE,
-//       IndexName: "albumSong",
-//       KeyConditionExpression: "album = :album",
-//       ExpressionAttributeValues: {
-//         ":album": album,
-//       },
-//     };
-
-//     queryDynamoDb(params)
-//       .then((items) => {
-//         if (items.Count < 1) {
-//           return res.status(404).send(`No songs found for album ${album}`);
-//         }
-//         return res
-//           .status(200)
-//           .send(_.uniq(_.map(items.Items, (item) => item.song)));
-//       })
-//       .catch((err) => {
-//         return res.status(500).send(err);
-//       });
-//   }
-// );
-
-// app.get(
-//   "/song",
-//   [
-//     query("song", "Missing parameter for song query").exists({
-//       checkFalsy: true,
-//     }),
-//   ],
-//   (req, res) => {
-//     const validationErrors = validationResult(req);
-//     if (!validationErrors.isEmpty()) {
-//       return res.status(400).send(validationErrors.array());
-//     }
-
-//     const song = req.query.song;
-
-//     const params = {
-//       TableName: MUSIC_TABLE,
-//       FilterExpression: "song = :song",
-//       ExpressionAttributeValues: { ":song": song },
-//     };
-
-//     scanDynamoDb(params)
-//       .then((items) => {
-//         if (items.Count < 1) {
-//           return res.status(404).send(`Song ${song} not found`);
-//         }
-//         return getSignedUrl(items.Items[0].s3Key);
-//       })
-//       .then((url) => {
-//         return res.status(200).send({ url });
-//       })
-//       .catch((err) => {
-//         return res.status(500).send(err);
-//       });
-//   }
-// );
+  scanDynamoDb(params)
+    .then((items) => {
+      if (items.Count < 1) {
+        return res.status(404).send(`Song ${song} not found`);
+      }
+      return getSignedUrl(items.Items[0].s3Key);
+    })
+    .then((url) => {
+      return res.status(200).send({ url });
+    })
+    .catch((err) => {
+      return res.status(500).send(err);
+    });
+});
 
 // app.post("/save-user", (req, res) => {
 //   const params = {
